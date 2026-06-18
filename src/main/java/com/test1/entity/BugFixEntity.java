b9cd23b8075e87ba449a0912ba2671475c80ac62
N.Suleman <naddimullasuleman@gmail.com>
2026-06-18 15:10:18 +0530

    Update BugFixEntity.java

--- src/main/java/com/test1/entity/BugFixEntity.java
+++ src/main/java/com/test1/entity/BugFixEntity.java
@@ -3,11 +3,13 @@ package com.test1.entity;
 public class BugFixEntity {
     public static void main(String[] args) {
         BugFixEntity bf = new BugFixEntity();
-        bf.test(100, 10.8, 200);
+        bf.test(100, 10.8, 200,500);
     }
-    private void test(int i, double d, int y) {
-        System.out.println(i);
-        System.out.println(d);
-        System.out.println(y);
+    private void test(int ...x) {
+        System.out.println(x[0]);
+        System.out.println(x[1]);
+        System.out.println(x[2]);
+         System.out.println(x[3]);
+
     }
 }
