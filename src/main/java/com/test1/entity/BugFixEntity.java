4cb66264c289e8152a2307d1d42ad67ca2dbc212
suleman <naddimullasuleman@gmail.com>
2026-06-18 15:30:30 +0530

    Integer a added

--- src/main/java/com/test1/entity/BugFixEntity.java
+++ src/main/java/com/test1/entity/BugFixEntity.java
@@ -3,13 +3,14 @@ package com.test1.entity;
 public class BugFixEntity {
     public static void main(String[] args) {
         BugFixEntity bf = new BugFixEntity();
-        bf.test(100, 20.3, true,500);
+        bf.test(100, 20.3, true,500, 800);
     }
-    private void test(int x,double d, boolean bool, int y ) {
+    private void test(int x,double d, boolean bool, int y, int a) {
         System.out.println(x);
         System.out.println(d);
         System.out.println(bool);
-         System.out.println(y);
+        System.out.println(y);
+        System.out.println(a);
 
     }
 }
